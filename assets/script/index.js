2b34f7dad003c8f86336fe3bc8f1e025b71c20b6
Islam Iktislyamov <islamikt07@gmail.com>
2022-12-13 09:42:10 -0800

    second commit

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,13 +1,51 @@
 'use strict'
 
-
-import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
-
+const overlay = document.querySelector('.overlay')
 
 mapboxgl.accessToken = 'pk.eyJ1IjoicGVjY2F0cml4MTMiLCJhIjoiY2xibWQ0NjlwMGczeTNvcDZ6cDdnOTFxZCJ9.5Cpc9H58djOl502MYhnFWw';
+
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9, // starting zoom
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11', 
+    center: [0, 0],
+    pitch: 40,
+    zoom: 16,
+    interactive: false
 });
+
+map.dragPan.disable();
+map.keyboard.disable();
+map.scrollZoom.disable();
+map.doubleClickZoom.disable();
+map.touchZoomRotate.disable();
+
+const marker = new mapboxgl.Marker({
+    color: '#DD0426'
+})
+
+function getLocation(position){
+    const {longitude, latitude} = position.coords;
+    
+    if (longitude && latitude){
+        map.setCenter([longitude, latitude]);
+        marker.setLngLat([longitude, latitude]).addTo(map);
+        setTimeout(() => {overlay.style.display = ' none'}, 750);
+    }
+}
+
+function errorHandler(event){
+    load.style.animationPlayState = 'paused';
+    console.log(event.message);
+}
+
+const options = {
+    enableHighAccuracy: true,
+    maximumAge: 0
+}
+
+if (navigator.geolocation){
+    navigator.geolocation.watchPosition(getLocation, errorHandler, options);
+} 
+else{
+    console.log('Geolocation is not supported by your browser')
+}
